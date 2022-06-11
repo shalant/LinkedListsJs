5f4494e361c233ac9149f38f88565f105cd78e70
shalant <doug.rosenberg@gmail.com>
2022-06-10 22:20:42 -0500

    2700

--- linked-list-values.js
+++ linked-list-values.js
@@ -5,24 +5,3 @@ class Node {
         this.next = null;
     }
 }
-
-const a = new Node('A');
-const b = new Node('B');
-const c = new Node('C');
-const d = new Node('D');
-
-a.next = b;
-b.next = c;
-c.next = d;
-
-/// A --> B --> C --> D --> NULL
-
-const printLinkedList = (head) => {
-    let current = head;
-    while (current != null) {
-        console.log(current.val);
-        current = current.next;
-    }
-};
-
-printLinkedList(a);
